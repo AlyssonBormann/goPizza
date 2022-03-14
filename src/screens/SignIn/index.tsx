165b77d0a469e054cffa8fb86a0d1ced552dbc33
Alysson Bormann <alysson.bormann@gmail.com>
2022-03-14 19:44:12 -0300

    Screen signIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,23 +1,44 @@
 import React from "react";
+import { KeyboardAvoidingView, Keyboard, Platform } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
-import { Container } from "./styles";
+import brandImg from "@assets/brand.png";
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgotPasswordButton,
+  ForgotPasswordLabel,
+} from "./styles";
 
 export function SignIn() {
   return (
     <Container>
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        enabled
+      >
+        <Content>
+          <Brand source={brandImg} />
+          <Title>Login</Title>
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
 
-      <Input placeholder="Senha" type="secondary" secureTextEntry />
-
-      <Button title="Entrar" type="secondary" />
+          <Input placeholder="Senha" type="secondary" secureTextEntry />
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+          <Button title="Entrar" type="secondary" />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
