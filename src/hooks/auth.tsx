60866bc566677ee549c5ec81f2e3506d2d946e14
Alysson Bormann <alysson.bormann@gmail.com>
2022-03-15 17:14:50 -0300

    hook de auth finalizado

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,15 +1,141 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from "react";
+import auth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Alert } from "react-native";
 
-type AuthContextData = {};
+type User = {
+  id: string;
+  name: string;
+  isAdmin: boolean;
+};
+
+type AuthContextData = {
+  signIn: (email: string, password) => Promise<void>;
+  signOut: () => Promise<void>;
+  forgotPassword: (email: string) => Promise<void>;
+  isLogging: boolean;
+  user: User | null;
+};
 
 type AuthProviderProps = {
   children: ReactNode;
 };
 
+const USER_COLLECTION = "@gopizza:users";
+
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [user, setUser] = useState<User | null>(null);
+  const [isLogging, setIsLogging] = useState(false);
+
+  async function signIn(email: string, password: string) {
+    if (!email || !password) {
+      return Alert.alert("Login", "Informe o e-mail e a senha.");
+    }
+
+    setIsLogging(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((account) => {
+        firestore()
+          .collection("users")
+          .doc(account.user.uid)
+          .get()
+          .then(async (profile) => {
+            const { name, isAdmin } = profile.data() as User;
+
+            if (profile.exists) {
+              const userData = {
+                id: account.user.uid,
+                name,
+                isAdmin,
+              };
+
+              await AsyncStorage.setItem(
+                USER_COLLECTION,
+                JSON.stringify(userData)
+              );
+              setUser(userData);
+            }
+          })
+          .catch(() =>
+            Alert.alert(
+              "Não foi possível buscar os dados de perfil do usuário."
+            )
+          );
+      })
+      .catch((error) => {
+        const { code } = error;
+
+        if (code === "auth/user-not-found" || code === "auth/wrong-password") {
+          return Alert.alert("Login", "E-mail e/ou senha inválido.");
+        } else {
+          Alert.alert("Login", "Não foi possível realizar o login.");
+        }
+      })
+      .finally(() => setIsLogging(false));
+  }
+
+  async function loadUserStorageData() {
+    setIsLogging(true);
+
+    const storedUser = await AsyncStorage.getItem(USER_COLLECTION);
+
+    if (storedUser) {
+      const userData = JSON.parse(storedUser);
+      setUser(userData);
+    }
+
+    setIsLogging(false);
+  }
+
+  async function signOut() {
+    await auth().signOut();
+    await AsyncStorage.removeItem(USER_COLLECTION);
+    setUser(null);
+  }
+
+  async function forgotPassword(email: string) {
+    if (!email) {
+      Alert.alert("Redifinir senha", "Informe o e-mail.");
+    }
+
+    auth()
+      .sendPasswordResetEmail(email)
+      .then(() => {
+        Alert.alert(
+          "Redefinir senha",
+          "Enivamos um link no seu e-mail para redefinir a senha."
+        );
+      })
+      .catch(() => {
+        Alert.alert(
+          "Redefinir senha",
+          "Não foi possível enviar o e-mail para redefinir a senha."
+        );
+      });
+  }
+
+  useEffect(() => {
+    loadUserStorageData();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ signIn, signOut, forgotPassword, isLogging, user }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { KeyboardAvoidingView, Keyboard, Platform } from "react-native";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+
+import { useAuth } from "@hooks/auth";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
@@ -16,6 +18,18 @@ import {
 } from "./styles";
 
 export function SignIn() {
+  const { signIn, forgotPassword, isLogging } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignIn() {
+    signIn(email, password);
+  }
+
+  function handleForgotPassword() {
+    forgotPassword(email);
+  }
+
   return (
     <Container>
       <KeyboardAvoidingView
@@ -30,13 +44,24 @@ export function SignIn() {
             type="secondary"
             autoCorrect={false}
             autoCapitalize="none"
+            onChangeText={setEmail}
           />
 
-          <Input placeholder="Senha" type="secondary" secureTextEntry />
-          <ForgotPasswordButton>
+          <Input
+            placeholder="Senha"
+            type="secondary"
+            secureTextEntry
+            onChangeText={setPassword}
+          />
+          <ForgotPasswordButton onPress={handleForgotPassword}>
             <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
           </ForgotPasswordButton>
-          <Button title="Entrar" type="secondary" />
+          <Button
+            title="Entrar"
+            type="secondary"
+            onPress={handleSignIn}
+            isLoading={isLogging}
+          />
         </Content>
       </KeyboardAvoidingView>
     </Container>
