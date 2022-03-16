d5717c534db472f5734fdab5e10ce6467d5a49e9
Alysson Bormann <alysson.bormann@gmail.com>
2022-03-16 19:10:50 -0300

    component search

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 
 import theme from "./src/theme";
 import { AuthProvider } from "@hooks/auth";
-import { Product } from "@screens/Product";
+import { Home } from "@screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +27,7 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <StatusBar style="light" translucent backgroundColor="transparent" />
         <AuthProvider>
-          <Product />
+          <Home />
         </AuthProvider>
       </ThemeProvider>
     </GestureHandlerRootView>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 
-import { Container } from "./styles";
+import happyEmoji from "@assets/happy.png";
+import {
+  Container,
+  Header,
+  Greeting,
+  GreetingEmoji,
+  GreetingText,
+} from "./styles";
+import { useTheme } from "styled-components";
 
 export function Home() {
-  return <Container></Container>;
+  const { COLORS } = useTheme();
+
+  return (
+    <Container>
+      <Header>
+        <Greeting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Olá, Admin meu truta!</GreetingText>
+        </Greeting>
+        <TouchableOpacity>
+          <MaterialIcons name="logout" color={COLORS.TITLE} sizse={24} />
+        </TouchableOpacity>
+      </Header>
+    </Container>
+  );
 }
